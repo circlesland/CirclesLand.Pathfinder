c317a9dc640f33dc2d8e5fa478080e0b12a4dd02
daniel <4954577+jaensen@users.noreply.github.com>
2024-05-17 16:23:21 +0200

    removed dead code

--- CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
+++ CirclesUBI.PathfinderUpdater.IndexerDb/Queries.cs
@@ -8,21 +8,10 @@ public static class Queries
         where safe_address != '0x0000000000000000000000000000000000000000'
         and balance > 0;
     ";
-    
-    public const string BalancesBySafeAndTokenAt25476000 = @"
-        select safe_address, token_owner, balance::text
-        from crc_balances_by_safe_and_token_at25476000
-        where safe_address != '0x0000000000000000000000000000000000000000';
-    ";
 
     public const string TrustEdges = @"
         select ""user"", ""can_send_to"", ""limit""
         from cache_crc_current_trust;
-    ";
-
-    public const string TrustEdgesAt25476000 = @"
-        select ""user"", ""can_send_to"", ""limit""
-        from cache_crc_current_trust_at25476000;
     ";
 
     public const string Users = @"
@@ -30,34 +19,13 @@ public static class Queries
         from crc_all_signups;
     ";
 
-    public const string UsersAt25476000 = @"
-        select ""user"", ""token""
-        from crc_all_signups_at25476000;
-    ";
-
     public static string BlockByTransactionHash(string txHash)
     {
         return $"select block_number from transaction_2 where hash = '{txHash}';";
     }
 
-    // public const string CapacityGraph = @"
-    //             select token_holder, can_send_to, token_owner, capacity::text
-    //             from crc_capacity_graph
-    //             where capacity > 0;";
-
-    public const string CapacityGraph = @"
-                select ""from"", ""to"", token_owner, capacity::text
-                from crc_capacity_graph_2
-                where capacity > 0;";
-
-    public const string CapacityGraphAt25476000 = @"
-                select ""from"", ""to"", token_owner, capacity::text
-                from crc_capacity_graph_at25476000
-                where capacity > 0;";
-
     public static string GetChanges(long sinceBlock)
     {
         return $"select token_holder, can_send_to, token_owner, capacity::text from get_capacity_changes_since_block_2({sinceBlock});";
-        // return $"select token_holder, can_send_to, token_owner, capacity::text from get_capacity_changes_since_block({sinceBlock});";
     }
 }
